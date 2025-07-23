03bf1e1dbb7c656cfe84cd771a913e7325499896
jcari-dev <582119jc@gmail.com>
2025-07-23 15:25:57 -0400

    made some changes

--- src/data/siteDetails.ts
+++ src/data/siteDetails.ts
@@ -2,7 +2,7 @@ export const siteDetails = {
     siteName: 'RoleRadar',
     siteUrl: 'https://www.roleradar.net/',
     metadata: {
-        title: 'RoleRadar - Next.js and Tailwind CSS Landing Page Template',
+        title: 'RoleRadar',
         description: 'RoleRadar empowers businesses with cutting-edge technology solutions to drive success and efficiency.',
     },
     language: 'en-us',
